040744fc7ba8cf5db16f6f2dd2ace6bb4b5347a1
zigomir <zigomir@gmail.com>
2013-12-05 16:08:21 +0100

    developerz

--- js/developer.js
+++ js/developer.js
@@ -1,14 +1,12 @@
 // Developer character
-
 function Developer(type) {
   this.type = type;
   this.hp   = 100;
   this.cost = 10;
 
-
   this.animation = new $.gQ.Animation({
-    imageURL: "img/sprite.png",
-    numberOfFrame: 2,
+    imageURL: "img/sprite_developer.png",
+    numberOfFrame: 3,
     delta: 16,
     rate: 90,
     type: $.gQ.ANIMATION_HORIZONTAL
@@ -17,7 +15,7 @@ function Developer(type) {
   this.getSprite = function() {
     return {
       animation: this.animation,
-      height: 16,
+      height: 28,
       width: 16
     };
   }
