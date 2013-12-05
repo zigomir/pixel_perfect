a0c85d4770c80794b725fec8fce9cbb1342335a0
zigomir <zigomir@gmail.com>
2013-12-05 15:31:13 +0100

    scene

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
 $(function() {
 
-  console.log("Let's do this!")
+  var $playground = $("#playground");
+
+  $playground.playground({
+    height: 800,
+    width: 600,
+    refreshRate: 60
+  })
+  .addSprite("sprite1");
+
+
+  $playground.startGame(function(){
+    console.log("Game started!")
+  });
 
 });
